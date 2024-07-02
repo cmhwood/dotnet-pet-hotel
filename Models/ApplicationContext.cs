198d570fa08447908748b9e773168f4c7e557aae
Adahlen1993 <156604329+Adahlen1993@users.noreply.github.com>
2024-07-02 15:39:33 -0500

    add db stuff

--- Models/ApplicationContext.cs
+++ Models/ApplicationContext.cs
@@ -6,4 +6,5 @@ public class ApplicationContext : DbContext
 {
   public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
   public DbSet<PetOwner> PetOwners { get; set; }
+  public DbSet<Pet> Pets { get; set; }
 }
