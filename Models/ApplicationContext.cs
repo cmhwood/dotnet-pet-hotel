16ad4ca89cc216738f97e4bd2aa529e6e6141742
Chris Harwood <cmharwood79@gmail.com>
2024-07-02 15:31:02 -0500

    created pet owner model

--- Models/ApplicationContext.cs
+++ Models/ApplicationContext.cs
@@ -5,5 +5,5 @@ namespace pet_hotel.Models;
 public class ApplicationContext : DbContext
 {
   public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
-  // Refer to example project: wire up your models here
+  public DbSet<PetOwner> PetOwners { get; set; }
 }
--- Models/PetOwner.cs
+++ Models/PetOwner.cs
@@ -1,10 +1,29 @@
 using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 using System.ComponentModel.DataAnnotations.Schema;
+using pet_hotel.Controllers;
 
 namespace pet_hotel.Models;
 
 public class PetOwner
 {
-   // Fields go here
+   public int Id { get; set; }
+
+   [Required]
+   public string Name { get; set; }
+
+   [Required]
+   public string Email { get; set; }
+
+   [JsonIgnore]
+   public ICollection<Pet> Pets { get; set; }
+
+   [NotMapped]
+   public int PetCount
+   {
+      get
+      {
+         return (Pets != null) ? Pets.Count : 0;
+      }
+   }
 }
