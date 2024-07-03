3075e63aaa92b27191db2282df2936323976f75e
Chris Harwood <cmharwood79@gmail.com>
2024-07-03 10:57:27 -0500

    database update

---
+++ Migrations/20240703155651_changeTableId.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace pet_hotel_7._0.Migrations
+{
+    /// <inheritdoc />
+    public partial class changeTableId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "PetOwners",
+                newName: "name");
+
+            migrationBuilder.RenameColumn(
+                name: "Email",
+                table: "PetOwners",
+                newName: "email");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "PetOwners",
+                newName: "id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "name",
+                table: "PetOwners",
+                newName: "Name");
+
+            migrationBuilder.RenameColumn(
+                name: "email",
+                table: "PetOwners",
+                newName: "Email");
+
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "PetOwners",
+                newName: "Id");
+        }
+    }
+}
