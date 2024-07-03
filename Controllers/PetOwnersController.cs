376d739444eeb90f2c463f9f8ee4a8b3648f6eea
Chris Harwood <cmharwood79@gmail.com>
2024-07-03 09:23:58 -0500

    updated pet owner controller
    Co-authored-by: Gabrielle Lommel <granolagabrielle@users.noreply.github.com>

--- Controllers/PetOwnersController.cs
+++ Controllers/PetOwnersController.cs
@@ -14,7 +14,54 @@ public class PetOwnersController : ControllerBase
       _context = context;
    }
 
-   // routes go here
+[HttpGet]
+public IEnumerable<PetOwner> GetPetOwners()
+{
+   return _context.PetOwners.Include(PetOwner => PetOwner.Pets).ToList();
+}
 
+[HttpGet("{PetOwnerId}")]
+public IActionResult GetOwnerById(int PetOwnerId)
+{
+   PetOwner petOwner = _context.PetOwners.Include(p => p.Pets).SingleOrDefault(petOwner => petOwner.Id == PetOwnerId);
+   if (petOwner == null)
+   {
+      return NotFound();
+   }
+   return Ok(petOwner);
+}
+
+[HttpPost]
+public IActionResult CreatePetOwner([FromBody] PetOwner newPetOwner) {
+   _context.PetOwners.Add(newPetOwner);
+   _context.SaveChanges();
+   return CreatedAtAction("/", new { Id = newPetOwner.Id }, newPetOwner);
+}
+
+[HttpDelete("{PetOwnerId}")]
+public IActionResult DeletePetOwnerById(int PetOwnerId)
+{
+   PetOwner petOwner = _context.PetOwners.SingleOrDefault(petOwner => petOwner.Id == PetOwnerId);
+   if (petOwner == null) return NotFound();
+
+   _context.PetOwners.Remove(petOwner);
+   _context.SaveChanges();
+
+   return NoContent();
+}  
+
+[HttpPut("{petOwnerId}")]
+public IActionResult UpdatePetOwnerById(int petOwnerId, [FromBody] PetOwner updatedPetOwner)
+{
+   PetOwner petOwner = _context.PetOwners.SingleOrDefault(petOwner => petOwner.Id == petOwnerId);
+   if (petOwner ==null) return NotFound();
+
+   petOwner.Name = updatedPetOwner.Name;
+
+   _context.PetOwners.Update(petOwner);
+   _context.SaveChanges();
+   
+   return Ok(petOwner);
+}
 
 }
